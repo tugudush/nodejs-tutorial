b39474dd2599189a19d8e44ce48b490952b682ba
tugudush <jerome2kph@gmail.com>
2018-10-15 23:51:12 +0800

    testing lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,11 +2,15 @@ console.log('starting app.js');
 
 const notes = require('./notes.js');
 const math = require('./math.js');
+const _ = require('lodash');
 
-notes.add_note();
+//notes.add_note();
 
 var a = 5;
 var b = 6;
 
 var sum = math.add(a, b);
-console.log(`${a} + ${b} = ${sum}`);
+//console.log(`${a} + ${b} = ${sum}`);
+
+var string_test = true;
+console.log(`is ${string_test} a string? ${_.isString(string_test)}`);
