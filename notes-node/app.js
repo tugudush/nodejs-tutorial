c938f54261c0e3eafc0d2bdb5db4f3a53b30582a
tugudush <jerome2kph@gmail.com>
2018-10-16 18:58:57 +0800

    added moment package

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,13 +4,13 @@ const notes = require('./notes.js');
 const math = require('./math.js');
 const _ = require('lodash');
 
-//notes.add_note();
+notes.add_note();
 
 var a = 5;
 var b = 6;
 
 var sum = math.add(a, b);
-//console.log(`${a} + ${b} = ${sum}`);
+console.log(`${a} + ${b} = ${sum}`);
 
-var string_test = true;
+var string_test = "shit";
 console.log(`is ${string_test} a string? ${_.isString(string_test)}`);
