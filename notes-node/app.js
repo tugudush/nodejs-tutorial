5e84cd114bc642dd2f46c060294a8ff8613f3b11
tugudush <jerome2kph@gmail.com>
2018-10-16 19:47:22 +0800

    added nodemon package

--- notes-node/app.js
+++ notes-node/app.js
@@ -12,5 +12,11 @@ var b = 6;
 var sum = math.add(a, b);
 console.log(`${a} + ${b} = ${sum}`);
 
-var string_test = "shit";
+var string_test = "hey";
 console.log(`is ${string_test} a string? ${_.isString(string_test)}`);
+
+var array = ['hi', 'hello', 1, 2, 3, 'hi', 2];
+console.log(`array: ${array}`);
+
+var filtered_array = _.uniq(array);
+console.log(`filtered_array: ${filtered_array}`);
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,11 +2,13 @@ console.log('starting notes.js');
 
 const fs = require('fs');
 const os = require('os');
+const moment = require('moment');
 
 var user = os.userInfo();
 var username = user.username;
+var now = moment();
 
 module.exports.add_note = function() {
   console.log('adding note to greetings.txt');
-  fs.appendFileSync('greetings.txt', `Hello ${username}!\r\n`);
+  fs.appendFileSync('greetings.txt', `Hi ${username}! Today is ${now}.\r\n`);
 }; // End of module.exports.add_note = function()
